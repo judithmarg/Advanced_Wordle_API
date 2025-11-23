f8ae9fdef3605b2ba7c97ac5139ebb58f4ecc0ec
judithMarg <judithmargaritapacollalli@gmail.com>
2025-11-23 19:10:23 -0400

    feat: create project

---
+++ src/main/java/com/beproject/wordleapi/WordleapiApplication.java
@@ -0,0 +1,13 @@
+package com.beproject.wordleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beproject/wordleapi/WordleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beproject.wordleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
