5b2bf99152cb8a07e369d70747d46f02a35d020b
judithMarg <judithmargaritapacollalli@gmail.com>
2025-11-30 23:27:42 -0400

    feat: add entities to accorsing the structure planified

---
+++ src/main/java/com/beproject/wordleapi/domain/entity/PressedLetter.java
@@ -0,0 +1,21 @@
+package com.beproject.wordleapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "pressed_letter", uniqueConstraints = @UniqueConstraint(columnNames = {"game_session_id", "letter"}))
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PressedLetter extends EventGame{
+
+    @Column(length = 1, nullable = false)
+    private char letter;
+
+    @Column(length = 20)
+    private String status;
+
+}
