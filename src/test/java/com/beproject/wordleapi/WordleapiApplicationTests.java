239b4232048bd230bc3b79d9a74a8ad26b3b7a99
LuciaBeVI <131404402+LuciaBeVI@users.noreply.github.com>
2025-12-02 21:47:36 -0400

    Update WordleapiApplicationTests.java

--- src/test/java/com/beproject/wordleapi/WordleapiApplicationTests.java
+++ src/test/java/com/beproject/wordleapi/WordleapiApplicationTests.java
@@ -1,11 +1,7 @@
 package com.beproject.wordleapi;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@Disabled
 class WordleapiApplicationTests {
 
 	@Test
